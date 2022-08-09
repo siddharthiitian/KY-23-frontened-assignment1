cb705ac844e8fb03e07c13390ee669a58cbdabbf
siddharthiitian <siddharth.prakash.bme21@itbhu.ac.in>
2022-08-10 01:12:33 +0530

    created
    kashiyatra

---
+++ src/components/SliderData.js
@@ -0,0 +1,16 @@
+export const SliderData=[
+    {
+        image:'https://images.unsplash.com/photo-1659733683446-5837c203be5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60',
+        des:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    },
+    {
+        image:'https://images.unsplash.com/photo-1657299142018-4f7f33aea18c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1659422530627-a2a429733450?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1659733478370-159f42ab3190?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60'
+    }
+];
